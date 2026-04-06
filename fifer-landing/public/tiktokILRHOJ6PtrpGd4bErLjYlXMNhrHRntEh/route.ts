8efe4e02ec3f1358e43da0602f60bfe052d726df
Cristóbal Kupfer <caks2257@gmail.com>
2026-04-06 09:10:43 -0400

    Arquitectura final: Solo archivo estático TXT en public

--- fifer-landing/public/tiktokILRHOJ6PtrpGd4bErLjYlXMNhrHRntEh/route.ts
+++
@@ -1,5 +0,0 @@
-export async function GET() {
-    return new Response('EL_CONTENIDO_DEL_ARCHIVO_DE_TIKTOK', {
-      headers: { 'content-type': 'text/plain' },
-    });
-  }
--- fifer-landing/src/app/tiktokILRHOJ6PtrpGd4bErLjYlXMNhrHRntEh/route.ts
+++
@@ -1,7 +0,0 @@
-export async function GET() {
-  return new Response("tiktok-developers-site-verification=ILRHOJ6PtrpGd4bErLjYlXMNhrHRntEh", {
-    headers: {
-      "Content-Type": "text/plain; charset=utf-8",
-    },
-  });
-}
