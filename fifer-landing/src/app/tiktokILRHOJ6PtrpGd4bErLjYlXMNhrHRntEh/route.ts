9cb8a9955c332cfaa853e0feb07f96f8f3d2c582
Cristóbal Kupfer <caks2257@gmail.com>
2026-04-06 08:55:57 -0400

    Arquitectura: Eliminando NextResponse no usado y corrigiendo ruta

---
+++ fifer-landing/src/app/tiktokILRHOJ6PtrpGd4bErLjYlXMNhrHRntEh/route.ts
@@ -0,0 +1,7 @@
+export async function GET() {
+  return new Response("tiktok-developers-site-verification=ILRHOJ6PtrpGd4bErLjYlXMNhrHRntEh", {
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8",
+    },
+  });
+}
