69cb47c57fd14a1533d6868b328b0a45d13c857d
Cristóbal Kupfer <caks2257@gmail.com>
2026-04-05 20:51:24 -0400

    fix: implementar meta-tag de Admitad y script de Impact

--- fifer-landing/src/app/layout.tsx
+++ fifer-landing/src/app/layout.tsx
@@ -35,7 +35,8 @@ export const metadata: Metadata = {
     description: siteDetails.metadata.description,
     images: ['/images/twitter-image.jpg'],
   },
-  // --- ADMITAD VERIFICATION ---
+  // --- VERIFICACIÓN DE ADMITAD ---
+  // Esto generará automáticamente: <meta name="verify-admitad" content="3099946505" />
   verification: {
     other: {
       'verify-admitad': '3099946505',
@@ -50,13 +51,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        {/* Next.js inyectará aquí automáticamente el meta tag de Admitad definido arriba */}
-      </head>
-      <body
-        className={`${manrope.className} ${sourceSans.className} antialiased`}
-      >
-        {siteDetails.googleAnalyticsId && <GoogleAnalytics gaId={siteDetails.googleAnalyticsId} />}
+      <body className={`${manrope.className} ${sourceSans.className} antialiased`}>
+        {siteDetails.googleAnalyticsId && (
+          <GoogleAnalytics gaId={siteDetails.googleAnalyticsId} />
+        )}
         
         <Header />
         <main>
@@ -67,7 +65,16 @@ export default function RootLayout({
         {/* --- IMPACT STAT TAG VERIFICATION --- */}
         <Script id="impact-stat-tag" strategy="afterInteractive">
           {`
-            (function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A7161370-3636-4aa4-8330-9e02253a92d91.js','script','impactStat',document,window);
+            (function(i,m,p,a,c,t){
+              c.ire_o=p;
+              c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};
+              t=a.createElement(m);
+              var z=a.getElementsByTagName(m)[0];
+              t.async=1;
+              t.src=i;
+              z.parentNode.insertBefore(t,z)
+            })('https://utt.impactcdn.com/P-A7161370-3636-4aa4-8330-9e02253a92d91.js','script','impactStat',document,window);
+            
             impactStat('transformLinks');
             impactStat('trackImpression');
           `}
