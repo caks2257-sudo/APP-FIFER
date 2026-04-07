0a20b0ff47c3fc3e0fdbbea2053b6c42942766be
Cristóbal Kupfer <caks2257@gmail.com>
2026-04-07 16:58:13 -0400

    feat: Implementar sincronización de etiquetas y procesamiento automático para actualizaciones de productos en Aliexpress

---
+++ deliverables/credits/credit_simulator.js
@@ -0,0 +1,5 @@
+/**
+ * Canonical implementation: ../../saas-fifer/modules/fifer-platform/credits/creditSimulator.js
+ * This file re-exports for deliverables packaging.
+ */
+module.exports = require('../../saas-fifer/modules/fifer-platform/credits/creditSimulator.js');
