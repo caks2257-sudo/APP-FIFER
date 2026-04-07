832b3b048d734b629c27f232e880790215e523f4
Cristóbal Kupfer <caks2257@gmail.com>
2026-04-07 16:59:04 -0400

    feat: Enhance Aliexpress product updates with improved tag synchronization and automated processing

---
+++ deliverables/scoring/scoring_engine.js
@@ -0,0 +1,5 @@
+/**
+ * Canonical implementation: ../../saas-fifer/modules/fifer-platform/scoring/scoringEngine.js
+ * This file re-exports for deliverables packaging.
+ */
+module.exports = require('../../saas-fifer/modules/fifer-platform/scoring/scoringEngine.js');
