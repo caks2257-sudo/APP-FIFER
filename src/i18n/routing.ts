577fd82604bc950eee9a2f8c0aff588c41628e06
Cristóbal Kupfer <caks2257@gmail.com>
2026-04-13 00:21:37 -0400

    fix: routing and add core services

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,13 +1,15 @@
 import {defineRouting} from 'next-intl/routing';
 import {createNavigation} from 'next-intl/navigation';
- 
+
 export const routing = defineRouting({
   // Lista de idiomas soportados
-  locales: ['en-US', 'es-CL'],
- 
+  locales: ['en-US', 'es-CL'] as const,
+
   // El idioma por defecto si el usuario entra a la raíz "/"
   defaultLocale: 'es-CL'
 });
- 
+
+export type AppLocale = (typeof routing.locales)[number];
+
 export const {Link, redirect, usePathname, useRouter} =
   createNavigation(routing);
